8435b76e73a1ce3603e776b2dd494be1fe1651ec
Andreas Schmidt <a.v.schmidt81@googlemail.com>
2020-04-13 19:27:06 +0200

    Documented the CAN Virtual Interface class and fixed a naming bug of the internal RX thread

--- lrapplications/tools/network_signals/network/can/can_virtual_interface.py
+++ lrapplications/tools/network_signals/network/can/can_virtual_interface.py
@@ -12,25 +12,33 @@ import time
 from pyusbtin.usbtin import CANMessage
 
 from util.logger import LoggerEntryType
-from util.logger_global import *
+from util.logger_global import defaultLog
 
-from network.can.can_interface import *
+from network.can.can_interface import CANInterface
 
 '''
 '''
 class CANVirtualInterface(CANInterface):
     '''
+    Represents a virtual CAN interface used for simulation purposes without a real
+    CAN connections
     '''
+
     def __init__(self):
+        '''
+        Initializes the Virtual CAN Interface
+        '''
         super().__init__("Virtual")
 
         self._stopRxThread = False
-        self._rxThread = threading.Thread(target=self._rxThread)
+        self._rxThread = threading.Thread(target=self._rxThreadFunction)
 
         self._canRxQueue = queue.Queue()
 
-    def _rxThread(self):
+    def _rxThreadFunction(self):
         '''
+        Function which is executed inside the internal RX thread which simulates the receiption of
+        CAN frames.
         '''
         while self._stopRxThread == False:
             canID = random.randrange(0x5FF, 0x610)
@@ -41,45 +49,52 @@ class CANVirtualInterface(CANInterface):
             self._receiveFrame(msg)
             time.sleep(0.1)
 
-    '''
-    '''
     def _receiveFrame(self, msg):
-        #print("Got Frame: " + str(msg))
+        '''
+        Adds the provided CAN frame to the internal RX qeueu
+        '''
         defaultLog(str(msg), LoggerEntryType.LOG_ACTIVITY)
         self._canRxQueue.put(msg)
 
-    '''
-    '''
     def getInterfaceObject(self):
+        '''
+        Returns a reference to it self because there is no internal interface object
+        '''
         return self
 
-    '''
-    '''
     def initializeInterface(self):
+        '''
+        Starts the internal RX thread
+        '''
         self._rxThread.start()
 
-    '''
-    '''
     def finalizeInterface(self):
+        '''
+        Stops the RX thread and waits till it finishes
+        '''
         self._stopRxThread = True
         self._rxThread.join()
 
     def getNextFrame(self):
         '''
+        Returns the next CAN frame from the internal RX queue.
         '''
         return self._canRxQueue.get(timeout=0.01)
 
     def hasNextFrame(self):
         '''
+        Returns true, if the internal RX queue has at least one CAN frame
         '''
         return not self._canRxQueue.empty()
 
     def sendFrame(self, frame):
         '''
+        Just adds a log message with the CAN Message to the logger backend
         '''
-        print("Sending frame from virtual interface: " + str(frame))
+        defaultLog("Sending virtual CAN Frame: " + str(frame), LoggerEntryType.LOG_ACTIVITY)
 
     def isOpen(self):
         '''
+        Returns alsways true for the virtual Interface
         '''
         return True
